daf9be7dafc799261ea293a940ee17336acba7c2
Uri Shaked <uri@urishaked.com>
2020-01-17 13:51:03 +0200

    feat: implement a basic spectra parser
    This should be useful for writing new tests, as it allows
    converting KiCad PCB files back into JavaScript objects

--- src/spectra.spec.ts
+++ src/spectra.spec.ts
@@ -1,11 +1,40 @@
-import { encodeObject } from './spectra';
+import { encodeObject, parseObject } from './spectra';
 
 describe('spectra', () => {
-  it('should encode the given object in Spectra format', () => {
-    expect(encodeObject(['foo', 'bar'])).toEqual('(foo bar)');
+  describe('encodeObject', () => {
+    it('should encode the given object in Spectra format', () => {
+      expect(encodeObject(['foo', 'bar'])).toEqual('(foo bar)');
+    });
+
+    it('should ignore null values', () => {
+      expect(encodeObject(['foo', null, 'bar'])).toEqual('(foo bar)');
+    });
   });
 
-  it('should ignore null values', () => {
-    expect(encodeObject(['foo', null, 'bar'])).toEqual('(foo bar)');
+  describe('parseObject', () => {
+    it('should parse a simple object', () => {
+      expect(parseObject('(foo bar)')).toEqual(['foo', 'bar']);
+    });
+
+    it('should parse nested objects with strings and numbers', () => {
+      expect(
+        parseObject(
+          '(fp_text user gge464 (at 0 0) (layer "Cmts.User") (effects (font (size 1 1) (thickness 0.15)))))'
+        )
+      ).toEqual([
+        'fp_text',
+        'user',
+        'gge464',
+        ['at', 0, 0],
+        ['layer', 'Cmts.User'],
+        ['effects', ['font', ['size', 1, 1], ['thickness', 0.15]]]
+      ]);
+    });
+
+    it('should parse embedded quotes in strings', () => {
+      expect(
+        parseObject(`(test "this string has embedded "" quotes inside" "this one doesn't")`)
+      ).toEqual(['test', 'this string has embedded " quotes inside', `this one doesn't`]);
+    });
   });
 });
--- src/spectra.ts
+++ src/spectra.ts
@@ -1,4 +1,6 @@
-interface ISpectraList extends Array<ISpectraList | string | number | null> {}
+export interface ISpectraList extends Array<ISpectraList | string | number | null> {}
+
+const WHITESPACE = [' ', '\t', '\r', '\n'];
 
 export function encodeString(str: string) {
   if (/^[a-z][a-z0-9_]+$/.test(str)) {
@@ -30,4 +32,59 @@ export function encodeObject(object: ISpectraList): string {
       .join(' ') +
     ')'
   );
+}
+function parseElement(input: string): [ISpectraList | string | number, number] {
+  let idx = 0;
+  while (WHITESPACE.includes(input[idx])) {
+    idx++;
+  }
+  if (idx >= input.length) {
+    throw new Error('Unexpected end of string');
+  }
+  if (input[idx] === '(') {
+    idx++;
+    const result = [];
+    while (input[idx] !== ')') {
+      if (idx >= input.length) {
+        throw new Error('Unexpected end of string');
+      }
+      const [element, len] = parseElement(input.substr(idx));
+      result.push(element);
+      idx += len;
+    }
+    return [result, idx + 1];
+  } else if (input[idx] === '"') {
+    idx++;
+    let result = '';
+    while (input[idx] !== '"') {
+      result += input[idx];
+      idx++;
+      if (input.substr(idx, 2) === '""') {
+        result += '"';
+        idx += 2;
+      }
+    }
+    return [result, idx + 1];
+  } else {
+    let result = '';
+    while (![...WHITESPACE, '(', ')'].includes(input[idx])) {
+      if (idx >= input.length) {
+        throw new Error('Unexpected end of string');
+      }
+      result += input[idx];
+      idx++;
+    }
+    const numVal = parseFloat(result);
+    if (typeof numVal === 'number' && !isNaN(numVal)) {
+      return [numVal, idx];
+    } else {
+      return [result, idx];
+    }
+  }
+}
+
+export function parseObject(spectra: string) {
+  spectra = spectra.trim();
+  const [parsed] = parseElement(spectra);
+  return parsed;
 }
