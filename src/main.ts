950e160ea0603aba57534522d74042f4002dfc63
Karin <karinch@users.noreply.github.com>
2020-01-29 22:50:58 +0200

    feat: initial schematic support - wires

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 
 import * as fs from 'fs';
 import { convertBoard } from './board';
+import { convertSchematic } from './schematic';
 
 if (process.argv.length < 3) {
   console.error(`Usage: ${process.argv[1]} <input.json> [output.kicad_pcb]`);
@@ -15,9 +16,10 @@ if (process.argv[2] === '-v') {
 }
 
 const input = JSON.parse(fs.readFileSync(process.argv[2], 'utf-8'));
+const output = input.docType === '5' ? convertSchematic(input) : convertBoard(input);
 const outputFile = process.argv[3];
 if (outputFile && outputFile !== '-') {
-  fs.writeFileSync(outputFile, convertBoard(input));
+  fs.writeFileSync(outputFile, output);
 } else {
-  process.stdout.write(convertBoard(input));
+  process.stdout.write(output);
 }
